b1ad63757894fc5404adc7ec463d4208d690e9d5
Wiebe Gouma <goumawiebe1234@proton.me>
2025-06-20 11:53:23 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+let slideIndex = 1;
+// slide laten zien op index 1, dus de eerste
+showSlides(slideIndex);
+
+// volgende / vorige
+// als je op volgende klikt, gaat die omhoog, anders omlaag
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    // laat slide zien die op index n staat
+    // voor de puntjes onderaan
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName('slides');
+    let dots = document.getElementsByClassName('dot');
+    // als n groter is dan aantal slides, 
+    // zet slideIndex weer op 1, en laat eerste zien
+    if (n > slides.length) {
+        slideIndex = 1
+    } if (n < 1) { // als de index lager dan 1 is, wordt het op de laatste gezet
+        slideIndex = slides.length
+    } // alle slides standaard op verbogen zetten
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    } // 'active' class verwijderen van alle dots
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    // toon geselecteerde slide
+    slides[slideIndex - 1].style.display = "block";
+    // voeg actief toe aan de bijbehorende dot
+    dots[slideIndex - 1].className += " active";
+}
+
+/*
+todo:
+- automatisch afspelen
+gebruik setInterval() om automatisch te wisselen, en mouseenter en mouseleave om te pauzeren
+
+- betere animates
+css 
+*/
